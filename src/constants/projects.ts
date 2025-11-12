34de397ec899b0192f659e7914c1748f51ca174a
Umesh Darlami <darlamiumesh123@gmail.com>
2025-11-12 18:28:11 +0545

    add projects

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -1,10 +1,11 @@
 import bcsthub from '@/assets/bcsithub.jpg';
 import travelAssistant from '@/assets/travel_assistant.jpg';
 import fastAndFuries from '@/assets/fast_and_furies.jpg';
+import ltgAdminPanel from '@/assets/ltg_admin.jpg';
 
 export const projects = [
   {
-    title: "Educational Platform",
+    title: "BCSITHub Online Learning Platform",
     description: "A full-featured online learning platform with courses, notes, past papers, student/admin dashboards, and secure access.",
     image: bcsthub,
     tech: ["React", "Node.js", "MongoDB", "Tailwind CSS"],
@@ -35,6 +36,17 @@ export const projects = [
     demo: "",
     features: ["Itinerary Planning", "Flight & Hotel Booking", "Real-Time Updates", "Interactive Chatbot", "Location-Based Suggestions", "Notifications & Reminders", "User Profiles", "Analytics Dashboard"],
     status: "In Progress",
+  },
+  {
+    title: "LTG admin panel",
+    description: "LTG admin panel which includes all the features like admin dashboard, tasks management, interns management, certificate management etc.",
+    image: ltgAdminPanel,
+    tech: ["React", "Node.js", "MongoDB", "Tailwind CSS", "Express", "Firebase", "backendless"],
+    category: "Web",
+    github: "https://github.com/UDM11/LTG_admin_Panel",
+    demo: "https://ltg-admin-b325a.web.app/",
+    features: ["Admin dashboard", "Tasks management", "Interns management", "Certificate management", "Secure login"],
+    status: "Completed",
   },
 ];
 
